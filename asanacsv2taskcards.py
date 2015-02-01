9abd3f18240d67250b75ef45565b6a59330d5b55
bomultimedia <bomultimedia@users.noreply.github.com>
2015-02-01 16:27:20 +1300

    Add coloured cards, severity, priority and effort tags

--- asanacsv2taskcards.py
+++ asanacsv2taskcards.py
@@ -23,57 +23,21 @@ id ID #REQUIRED>
 	   margin:0px 0px 10px 0px;padding:0px;
 	   width:260px;
 	   height:140px;
-	   border:2px solid #007fff;
+	   border:2px solid #AAAAAA;
 	   page-break-inside: avoid !important;
-
-	   -moz-border-radius-bottomleft:0px;
-	   -webkit-border-bottom-left-radius:0px;
-	   border-bottom-left-radius:0px;
-
-	   -moz-border-radius-bottomright:0px;
-	   -webkit-border-bottom-right-radius:0px;
-	   border-bottom-right-radius:0px;
-
-	   -moz-border-radius-topright:0px;
-	   -webkit-border-top-right-radius:0px;
-	   border-top-right-radius:0px;
-
-	   -moz-border-radius-topleft:0px;
-	   -webkit-border-top-left-radius:0px;
-	   border-top-left-radius:0px;
 	 }.TaskTable table{
 		 border-collapse: collapse;
 			 border-spacing: 0;
 	   width:100%;
 	   height:100%;
 	   margin:0px;padding:0px;
-	 }.TaskTable tr:last-child td:last-child {
-	   -moz-border-radius-bottomright:0px;
-	   -webkit-border-bottom-right-radius:0px;
-	   border-bottom-right-radius:0px;
-	 }
-	 .TaskTable table tr:first-child td:first-child {
-	   -moz-border-radius-topleft:0px;
-	   -webkit-border-top-left-radius:0px;
-	   border-top-left-radius:0px;
-	 }
-	 .TaskTable table tr:first-child td:last-child {
-	   -moz-border-radius-topright:0px;
-	   -webkit-border-top-right-radius:0px;
-	   border-top-right-radius:0px;
-	 }.TaskTable tr:last-child td:first-child{
-	   -moz-border-radius-bottomleft:0px;
-	   -webkit-border-bottom-left-radius:0px;
-	   border-bottom-left-radius:0px;
 	 }.TaskTable tr:hover td{
 	   background-color:#ffffff;
-
-
 	 }
 	 .TaskTable td{
 	   vertical-align:middle;
 	   background-color:#ffffff;
-	   border:0px solid #007fff;
+	   border:0px solid #AAAAAA;
 	   border-width:0px 0px 0px 0px;
 	   text-align:left;
 	   padding:7px;
@@ -89,12 +53,8 @@ id ID #REQUIRED>
 	   border-width:0px 0px 0px 0px;
 	 }
 	 .TaskTable tr:first-child td{
-		 background:-o-linear-gradient(bottom, #007fff 5%, #007fff 100%);	background:-webkit-gradient( linear, left top, left bottom, color-stop(0.05, #007fff), color-stop(1, #007fff) );
-	   background:-moz-linear-gradient( center top, #007fff 5%, #007fff 100% );
-	   filter:progid:DXImageTransform.Microsoft.gradient(startColorstr="#007fff", endColorstr="#007fff");	background: -o-linear-gradient(top,#007fff,007fff);
-
-	   background-color:#007fff;
-	   border:0px solid #007fff;
+	   background-color:#AAAAAA;
+	   border:0px solid #AAAAAA;
 	   text-align:center;
 	   border-width:0px 0px 0px 0px;
 	   font-size:14px;
@@ -103,11 +63,7 @@ id ID #REQUIRED>
 	   color:#ffffff;
 	 }
 	 .TaskTable tr:first-child:hover td{
-	   background:-o-linear-gradient(bottom, #007fff 5%, #007fff 100%);	background:-webkit-gradient( linear, left top, left bottom, color-stop(0.05, #007fff), color-stop(1, #007fff) );
-	   background:-moz-linear-gradient( center top, #007fff 5%, #007fff 100% );
-	   filter:progid:DXImageTransform.Microsoft.gradient(startColorstr="#007fff", endColorstr="#007fff");	background: -o-linear-gradient(top,#007fff,007fff);
-
-	   background-color:#007fff;
+	   background-color:#AAAAAA;
 	 }
 	 .TaskTable tr:first-child td:first-child{
 	   border-width:0px 0px 0px 0px;
@@ -121,6 +77,22 @@ id ID #REQUIRED>
 	   font-weight:normal;
 	   font-size:10px;
 	   height: 12px;
+	 }
+
+	 .TaskTable td.priority {
+	   padding: 5px 5px 5px 5px;
+	   font-weight:normal;
+	   font-size:10px;
+	   height: 12px;
+	   width: 10%
+	 }
+
+	 .TaskTable td.severity {
+	   padding: 5px 5px 5px 5px;
+	   font-weight:normal;
+	   font-size:10px;
+	   height: 12px;
+	   width: 10%
 	 }
 
 	 .TaskTable td.name {
@@ -140,33 +112,151 @@ id ID #REQUIRED>
 	   height: 10px;
 	 }
 
+	 .TaskTable td.effort {
+	   padding: 0px 5px 0px 5px;
+	   font-weight:normal;
+	   font-size:20px;
+	   text-align:right;
+	   color:#888;
+	   height: 20px;
+	 }
+
 	 .TaskTable td.parent {
 	   padding: 0px 5px 0px 5px;
 	   font-weight:normal;
 	   font-size:10px;
 	   height: 10px;
 	 }
+	 div.whitebadge{
+	   border: 0px solid;
+	   border-radius: 12px;
+	   background:white;
+	   padding:2px;
+	   margin:0px;
+	   font-weight:bold;
+	   color:#000;
+	}
+
+	.TaskTableGreen {
+	   border-color:#37B30D;
+	 }
+	 .TaskTableGreen td{
+	   border-color:#37B30D;
+	 }
+	 .TaskTableGreen tr:first-child td{
+
+	   background-color:#37B30D;
+	   border-color:#37B30D;
+	 }
+	 .TaskTableGreen tr:first-child:hover td{
+	   background-color:#37B30D;
+	 }
+
+
+	 .TaskTableOrange {
+	   border-color:#FF8000;
+	 }
+	 .TaskTableOrange td{
+	   border-color:#FF8000;
+	 }
+	 .TaskTableOrange tr:first-child td{
+
+	   background-color:#FF8000;
+	   border-color:#FF8000;
+	 }
+	 .TaskTableOrange tr:first-child:hover td{
+	   background-color:#FF8000;
+	 }
+
+	 .TaskTableYellow {
+	   border-color:#FFD527;
+	 }
+	 .TaskTableYellow td{
+	   border-color:#FFD527;
+	 }
+	 .TaskTableYellow tr:first-child td{
+
+	   background-color:#FFD527;
+	   border-color:#FFD527;
+	 }
+	 .TaskTableYellow tr:first-child:hover td{
+	   background-color:#FFD527;
+	 }
+
+	 .TaskTableRed {
+	   border-color:#FF0000;
+	 }
+	 .TaskTableRed td{
+	   border-color:#FF0000;
+	 }
+	 .TaskTableRed tr:first-child td{
+
+	   background-color:#FF0000;
+	   border-color:#FF0000;
+	 }
+	 .TaskTableRed tr:first-child:hover td{
+	   background-color:#FF0000;
+	 }
+
+	 .TaskTableBlue {
+	   border-color:#007FFF;
+	 }
+	 .TaskTableBlue td{
+	   border-color:#007FFF;
+	 }
+	 .TaskTableBlue tr:first-child td{
+
+	   background-color:#007FFF;
+	   border-color:#007FFF;
+	 }
+	 .TaskTableBlue tr:first-child:hover td{
+	   background-color:#007FFF;
+	 }
 
    </style>
   </head>
   <body>
   <xsl:for-each select="tasklist/task">
-	<div class="TaskTable" >
-	<table border="1">
+	 <div>
+		<xsl:choose>
+  			<xsl:when test='type = "PBI"'>
+				<xsl:attribute name="class">TaskTable TaskTableOrange</xsl:attribute>
+  			</xsl:when>
+  			<xsl:when test='type = "DOC"'>
+  				 <xsl:attribute name="class">TaskTable TaskTableGreen</xsl:attribute>
+  			</xsl:when>
+  			<xsl:when test='type = "DEV"'>
+  				 <xsl:attribute name="class">TaskTable TaskTableBlue</xsl:attribute>
+  			</xsl:when>
+  			<xsl:when test='type = "QA"'>
+  				 <xsl:attribute name="class">TaskTable TaskTableYellow</xsl:attribute>
+  			</xsl:when>
+  			<xsl:when test='type = "BUG"'>
+  				 <xsl:attribute name="class">TaskTable TaskTableRed</xsl:attribute>
+  			</xsl:when>
+  			<xsl:otherwise>
+				<xsl:attribute name="class">TaskTable</xsl:attribute>
+  			</xsl:otherwise>
+		</xsl:choose>
+
+		<table border="1">
 	  <tr >
+		<td class="priority"><div class="whitebadge"><xsl:value-of select="priority"/></div></td>
 		<td class="id"><xsl:value-of select="id"/></td>
+		<td class="severity"><div class="whitebadge"><xsl:value-of select="severity"/></div></td>
 	  </tr>
 	  <tr >
-		<td class="name"><xsl:value-of select="name"/></td>
+		<td colspan="3" class="name" ><xsl:value-of select="name"/></td>
 	  </tr>
 	  <tr>
-		<td class="empty"></td>
+		<td colspan="3" class="empty"></td>
 	  </tr>
 	  <tr >
-		<td class="projects">Projects:<xsl:value-of select="projects" /></td>
+		<td colspan="2" class="projects">Projects:<xsl:value-of select="projects" /></td>
+		<td rowspan="2" class="effort"><xsl:value-of select="effort"/></td>
 	  </tr>
 	  <tr >
-		<td class="parent">Parent:<xsl:value-of select="parent"/></td>
+		<td colspan="2" class="parent">Parent:<xsl:value-of select="parent"/></td>
 	  </tr>
 	</table>
 	</div>
@@ -194,8 +284,45 @@ with open(sys.argv[1], 'rb') as csvfile:
 		else:
 		    line_idx+=1
 		taskid,createdat,completedat,lastmodified,name,assignee,duedate,tags,notes,projects,parenttask=row
+		tasktype=None
+		taskpriority=None
+		taskseverity=None
+		taskeffort=None
+		if ((name.upper().startswith("BUG") or  
+		name.upper().startswith("QA") or 
+		name.upper().startswith("PBI") or
+		name.upper().startswith("DOC") or 
+		name.upper().startswith("DEV") or 
+		name.upper().startswith("BUG") ) and (name.find(":") >= 0)) :
+			if name.upper().startswith("BUG"):
+				tasktype="BUG"
+			if name.upper().startswith("PBI"):
+				tasktype="PBI"
+			elif name.upper().startswith("QA"):
+				tasktype="QA"
+			elif name.upper().startswith("DOC"):
+				tasktype="DOC"
+			elif name.upper().startswith("DEV"):
+				tasktype="DEV"
+			elif name.upper().startswith("BUG"):
+				tasktype="BUG"
+			if name.find("<")>=0 and name.find(">")>=0:
+				taskpriority=name[name.find("<")+1:name.find(">")]
+			if name.find("(")>=0 and name.find(")")>=0:
+				taskseverity=name[name.find("(")+1:name.find(")")]
+			if name.find("[")>=0 and name.find("]")>=0:
+				taskeffort=name[name.find("[")+1:name.find("]")]	
+			name=name[name.find(":")+1:]
 		print "  <task>"
 		print "    <id>"+taskid+"</id>"
+		if tasktype is not None:
+			print "    <type>"+tasktype+"</type>"
+		if taskpriority is not None:
+			print "    <priority>"+taskpriority+"</priority>"
+		if taskseverity is not None:
+			print "    <severity>"+taskseverity+"</severity>"
+		if taskeffort is not None:
+			print "    <effort>"+taskeffort+"</effort>"
 		print "    <name>"+name+"</name>"
 		print "    <projects>"+projects+"</projects>"
 		print "    <parent>"+parenttask+"</parent>"
